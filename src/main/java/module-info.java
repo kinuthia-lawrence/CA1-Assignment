ac4eb8bf0fe9030cd461774169ec6f9abf6befef
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2025-04-12 12:59:38 +0300

    basic setup

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.larrykin.ca1assignment {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.ikonli.javafx;
-
-    opens com.larrykin.ca1assignment to javafx.fxml;
-    exports com.larrykin.ca1assignment;
-}
